96e4f72cfd5a3079dcc1936eafec944ee39385e3
hseindirani <dirani.hussein93@gmail.com>
2025-11-10 11:21:51 +0100

    Add MappingProfiles for AutoMapper

---
+++ MovieReview/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MovieReview.Dto;
+using MovieReview.Models;
+
+namespace MovieReview.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Movie, MovieDto>();
+        }
+    }
+}
